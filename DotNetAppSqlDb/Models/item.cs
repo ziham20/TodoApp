142e3410c5b5fa30a648df8da26ba0ad1817c331
Ziham <zihamzuhair@hotmail.com>
2020-01-26 10:15:39 +0530

    Initial commit

---
+++ DotNetAppSqlDb/Models/item.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DotNetAppSqlDb.Models
+{
+    public class Item
+    {
+        public string itemId  { get; set; }
+        public string description { get; set; }
+        public string status_Id { get; set; }
+        public DateTime date { get; set; }
+        public string IpAddress { get; set; }
+    }
+}
